d09fe24a5cc764180518b0eb5c3dc319956c3e1c
edsonariel <developeritl@itl3pl.com>
2023-10-06 13:46:49 -0600

    regresando

--- transporta/app/login/page.tsx
+++ transporta/app/login/page.tsx
@@ -56,7 +56,7 @@ function Login(){
                     className="flex w-full justify-center rounded-md bg-anaranjado-transporta px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                 
-                    INICIO1234
+                    INICIO
                 </button>
 
             </Link>
